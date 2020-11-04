6aae442fabb98b6cdc48a83501750e5a503aff32
olivierargaud <olivier.argaud@ipilyon.net>
2020-11-04 11:13:18 +0100

    test des methodes EmployeRepository avec employe en abstract

--- src/main/java/com/ipiecoles/java/java230/MyRunner.java
+++ src/main/java/com/ipiecoles/java/java230/MyRunner.java
@@ -4,8 +4,13 @@ import com.ipiecoles.java.java230.model.Employe;
 import com.ipiecoles.java.java230.repository.EmployeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
 import java.util.Optional;
 
 @Component
@@ -17,26 +22,118 @@ public class MyRunner implements CommandLineRunner
     @Override
     public void run(String... strings) throws Exception
     {
-        // taper du code test ici
         System.out.println("je lance le test");
-        Long nbEmployes = employeRepository.count();
-        System.out.println("il y a " + nbEmployes + " dans la base de données");
-        Optional<Employe> employe = employeRepository.findById(55L);
-        if(employe.isEmpty())
+//////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////
+//                      pour decommenter cette section il faut que employe ne soit pas abstract
+//
+//        Long nbEmployes = employeRepository.count();
+//        System.out.println("il y a " + nbEmployes + " dans la base de données");
+//
+//
+//        // test recherche par index
+//        Optional<Employe> employe = employeRepository.findById(55L);
+//        if(employe.isEmpty())
+//        {
+//            System.out.println("Employe inexistant");
+//        }
+//        else
+//        {
+//            Employe e = employe.get();
+////            e.setSalaire(e.getSalaire()+200); // pour augmenter le salaire de l'employe 55
+////            e = employeRepository.save(e);
+//            System.out.println(employe.get().toString());
+//        }
+////        for (Employe emp : employeRepository.findAll())   // boucle pour afficher tous les employes
+////        {
+////            System.out.println(emp.toString());
+////        }
+//
+//
+//
+//        // test recherche par matricule
+        Employe emp1 = employeRepository.findByMatricule("C00004");
+        if (emp1 !=null)
         {
-            System.out.println("Employe inexistant");
+            System.out.println(emp1);
         }
         else
         {
-            Employe e = employe.get();
-//            e.setSalaire(e.getSalaire()+200); // pour augmenter le salaire de l'employe 55
-//            e = employeRepository.save(e);
-            System.out.println(employe.get().toString());
+            System.out.println("pas d'employé avec ce matricule");
         }
-//        for (Employe emp : employeRepository.findAll())   // boucle pour afficher tous les employes
+//
+//        // test recherche par nom et prenom
+//        List<Employe> employeList =employeRepository.findByNomAndPrenom("Andre","Alexandre");
+//        for(Employe employe1: employeList)
+//        {
+//            System.out.println(employe.toString());
+//        }
+//
+//        // test recherche par salaire avec filtre
+//        List<Employe> employeList2 =employeRepository.findBySalaireGreaterThanOrderBySalaireDesc(2400d);
+//        for(Employe employe2: employeList2)
+//        {
+//            System.out.println(employe.toString());
+//        }
+//
+//
+//        // test recherche par prenom avec trie ascendant et mise en page
+//        PageRequest pageRequest = PageRequest.of(0,5,Sort.Direction.ASC,"prenom");
+//        Page<Employe>employePage = employeRepository.findByNomIgnoreCase("Andre",pageRequest);
+//        System.out.println("nombre de page total : "+employePage.getTotalPages());
+//        System.out.println("nombre d'elements : "+employePage.getTotalElements());
+//        for(Employe employe3 : employePage)
+//        {
+//            System.out.println(employe3.toString());
+//        }
+
+
+
+//        // test recherche par nom ou prenom
+        List<Employe> employeList3 =employeRepository.findByNomOrPrenomAllIgnoreCase("Julien");
+        for(Employe employe3: employeList3)
+        {
+            System.out.println(employe3.toString());
+        }
+
+        // test recherche par nom ou prenom paginé
+//        PageRequest pageRequest3 = PageRequest.of(0,5);
+//        for(Employe emp : employeRepository.findByNomOrPrenomAllIgnoreCase("juLien",pageRequest3))
 //        {
 //            System.out.println(emp.toString());
 //        }
+
+
+
+
+        List<Employe>  emp4 = employeRepository.findEmployePlusRiches();
+        for(Employe employe4: emp4)
+        {
+            System.out.println(employe4.toString());
+        }
+
+
+
+
+
+
+
+
+//
+//        // mes test Recherche
+//        PageRequest pageRequest2 = PageRequest.of(0,5);
+//        for(Employe emp : employeRepository.findByPrenomIgnoreCase("gabRiel",pageRequest2))
+//        {
+//            System.out.println(emp.toString());
+//        }
+//        for(Employe emp : employeRepository.findByNomIgnoreCase("daviD",pageRequest2))
+//        {
+//            System.out.println(emp.toString());
+//        }
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////
+
     }
 
     public static void print(Object t) {
--- src/main/java/com/ipiecoles/java/java230/repository/EmployeRepository.java
+++ src/main/java/com/ipiecoles/java/java230/repository/EmployeRepository.java
@@ -2,33 +2,128 @@ package com.ipiecoles.java.java230.repository;
 
 import com.ipiecoles.java.java230.model.Employe;
 import org.joda.time.LocalDate;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 
-import java.util.ArrayList;
 import java.util.List;
 
-public interface EmployeRepository extends CrudRepository<Employe, Long>
+
+public interface EmployeRepository extends PagingAndSortingRepository<Employe, Long>
 {
-    public ArrayList<Employe> findByMatricule(String matricule);
 
-    public ArrayList<Employe> findByNomAndPrenom(String nom,String prenom);
+    Employe findByMatricule(String matricule);
 
-    public ArrayList<Employe>  findByNomIgnoreCase(String nom);
+    List<Employe> findByNomAndPrenom(String nom, String prenom);    // on utilise List pour rester generique
 
-    public ArrayList<Employe>  findByDateEmbaucheBefore(LocalDate date);
+    Page<Employe> findByPrenomIgnoreCase( String prenom, Pageable pageable);
 
-    public ArrayList<Employe>  findByDateEmbaucheAfter(LocalDate date);
+    List<Employe> findByNomIgnoreCase(String nom);
 
-    public ArrayList<Employe>  findBySalaireGreaterThanOrderBySalaireDesc(Double salaire);
+    Page<Employe> findByNomIgnoreCase(String nom, Pageable pageable);
+
+    List<Employe> findByDateEmbaucheBefore(LocalDate date);
+
+    List<Employe> findByDateEmbaucheAfter(LocalDate date);
+
+    List<Employe> findBySalaireGreaterThanOrderBySalaireDesc(Double salaire);
 
 
+//    version qui valide les tests mais fonctione pas
+//    @Query("select e from Employe e where lower(e.prenom) = lower(:nomOuPrenom) or lower(e.nom) = lower(:nomOuPrenom)")
+//    List<Employe> findByNomOrPrenomAllIgnoreCase(@Param("toto") String nomOuPrenom);
+
+
+//    corection
+
+    @Query("select e from Employe e where lower(e.prenom) = lower(:nomOuPrenom) or lower(e.nom) = lower(:nomOuPrenom)")
+    List<Employe> findByNomOrPrenomAllIgnoreCase(@Param("nomOuPrenom") String nomOuPrenom);
+
+
+
+
+
+
+
+
+
+////    ma version en list fonctionne
+//    @Query("from Employe where nom = lower(:toto) or prenom= lower(:toto)")
+//    List<Employe> findByNomOrPrenomAllIgnoreCase(@Param("toto") String nomOuPrenom);
 //
-//    public ArrayList<Employe> sortByAvantDate();
-//
-//    public ArrayList<Employe> sortByApresDate();
-//
-//    public ArrayList<Employe> sortBySalaire();
+////    ma version en page fonctionne pas
+//    @Query("from Employe where nom = lower(:toto) or prenom= lower(:toto)")
+//    Page<Employe> findByNomOrPrenomAllIgnoreCase(@Param("toto") String nomOuPrenom, Pageable pageable);
 
+
+
+//    @Query(value = "SELECT * FROM Employe WHERE salaire>2480", nativeQuery = true)
+//    List<Employe> findEmployePlusRiches();
+
+
+//  correction
+    @Query(value = "SELECT * FROM Employe WHERE salaire > (SELECT avg(e2.salaire)FROM Employe e2)", nativeQuery = true)
+    List<Employe> findEmployePlusRiches();
 
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//public interface VehiculeRepository extends CrudRepository<Vehicule,Integer> {
+//    //SELECT * FROM vehicule WHERE immatriculation = ?;
+//    Vehicule findByImmatriculation(String immatriculation);
+//    //SELECT * FROM vehicule WHERE marque = ? AND modele = ?;
+//    List<Vehicule> findByMarqueAndModele(String marque, String modele);
+//    //SELECT * FROM vehicule WHERE lower(marque) = ?;
+//    List<Vehicule> findByMarqueIgnoreCase(String nom);
+//    //Idem mais avec pagination
+//    Page<Vehicule> findByMarqueIgnoreCase(String nom, Pageable pageable);
+//    //SELECT * FROM vehicule WHERE date_mise_circulation < ?;
+//    List<Vehicule> findByDateMiseCirculationBefore(LocalDate date);
+//    //SELECT * FROM vehicule WHERE puissance > ? ORDER BY immatriculation DESC;
+//    List<Vehicule> findByPuissanceGreaterThanOrderByImmatriculationDesc(Integer puissance);
+//}
+
+
+// JPQL -> pseudo SQL pour les requetes un peut plus complexe
+//public interface VehiculeRepository extends CrudRepository<Vehicule,Integer> {
+//
+//    @Query("select v from Vehicule v where length(v.marque) = ? ")
+//    @Query("from Vehicule v where length(v.marque) = ? ")   select v n'est pas necessaire
+//    List<Vehicule> findByLongueurMarque(Integer longueur);
+//}
+
+// SQL natif quand on a plus le choix du tout
+//public interface ProprietaireRepository extends CrudRepository<Proprietaire,Integer> {
+//
+//    @Query(value = "SELECT p.* FROM proprietaire p " +
+//            "INNER JOIN vehicule v ON v.proprietaire_id = p.id " +
+//            "WHERE v.marque = :marque1 " +                        // on nomme le param plutot que de mettre "?"
+//            "AND EXISTS ( " +
+//            "SELECT p2.* FROM proprietaire p2 " +
+//            "INNER JOIN vehicule v2 ON v2.proprietaire_id = p2.id " +
+//            "WHERE p2.id = p.id" +
+//            "AND v2.marque = :marque2 " +
+//            ")", nativeQuery = true)
+//    List<Proprietaire> findByDeuxMarques(@Param("marque1") String marque1, @Param("marque2") String marque2);
+//}
